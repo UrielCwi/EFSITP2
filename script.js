fc690d5fdfb02a9f52bbfbc8a8c410039ebf1ecb
AXEL NICOLÁS KLEIMAN <47434251@ort>
2024-04-03 12:10:17 -0300

    2.0

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var Lista2 = [];
 var ListaHTML;
 var objeto={};
 var iterador = 1;
+let resta = null;
+let fechaRapida = null;
+let nombreTarea = null;
 function AgregarTODO(){
     objeto = {
         id: iterador,
@@ -13,9 +16,23 @@ function AgregarTODO(){
     }
    // Lista += Lista2;
    Lista.push(objeto);
-    CrearLista();
-    iterador++;
+   CrearLista();
+   iterador++;
 }
+
+function MostrarMasRapido(){
+    for (let index = 0; index < Lista.length; index++) {
+        if(fechaTachado != null){
+            resta = (fechaAñadida.getTime() - Lista[index].fechaTachado.getTime());
+            if(fechaRapida == null || resta < fechaRapida || resta == null) { //si tienen fecha de tachado Y es mas rapido que el mas rapido de los anteriores, entonces remplazarlo
+                fechaRapida = resta;
+                nombreTarea = Lista[index].informacion.value;
+            }
+        }
+    }         //si NO tiene fecha de tachado, entonces no cambiar nada
+}
+    //si el numero sigue siendo null, entonces mandar un mensaje de que no hay tareas finalizadas
+    //tene cuidado que creo que tenes que cambiar el formato de date.now()
 
 function CrearLista(){
     ListaHTML = "";
